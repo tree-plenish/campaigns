c684550060f6c4e4d1bc6efbc55afa7f360399a3
justinmiller33 <48934159+justinmiller33@users.noreply.github.com>
2021-06-22 14:12:47 -0400

    Trying logging &print to get web log access via aws

--- application.py
+++ application.py
@@ -5,6 +5,9 @@ application = flask.Flask(__name__)
 
 @application.route('/')
 def starting_url():
+    campaign = flask.request.args.get("c")
+    print(campaign)
+    application.logger.info('Campaign Number: %s', campaign)
     return flask.redirect('https://www.tree-plenish.org/host-an-event')
 
 if __name__ == '__main__':
