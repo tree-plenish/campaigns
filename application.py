edd7ded0bf019bfe43ca054c1dd0b998a54bcc4e
allisonkwan <akwan2020@gmail.com>
2021-07-08 15:49:44 -0400

    Remove Google Analytics; revert back to flask.redirect

--- application.py
+++ application.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
-#import flask
+#from flask import Flask, render_template
+import flask
 import logging
 
-application = Flask(__name__)
-#application = flask.Flask(__name__)
+#application = Flask(__name__)
+application = flask.Flask(__name__)
 #logging.basicConfig(filename="/var/log/web.stdout.log")
 #logging.basicConfig(filename="/var/log/campaigns.log")
 
@@ -13,8 +13,8 @@ def starting_url():
     #campaign = flask.request.args.get("c")
     # print(campaign)
     #application.logger.info('Campaign Number: %s', campaign)
-    return render_template('home.html')
-    #return flask.redirect('https://www.tree-plenish.org/host-an-event')
+    #return render_template('home.html')
+    return flask.redirect('https://www.tree-plenish.org/host-an-event')
 
 if __name__ == '__main__':
     application.run()
